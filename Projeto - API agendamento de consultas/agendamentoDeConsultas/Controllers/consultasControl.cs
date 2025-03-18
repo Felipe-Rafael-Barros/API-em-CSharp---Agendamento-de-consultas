fc84ed6bc0e2469d5bd13ec4c6a000fd1dd17683
Felipe_Barros <gitfelipebarros@gmail.com>
2025-03-18 17:47:15 -0300

    Atualizando API
    Implementando os primeiros Controllers para a API

---
+++ Projeto - API agendamento de consultas/agendamentoDeConsultas/Controllers/consultasControl.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using agendamentoDeConsultas.Models;
+
+namespace agendamentoDeConsultas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class consultasControl
+    {
+        private static List<Consultas> __consultas = new List<Consultas>();
+
+        
+    }
+}
